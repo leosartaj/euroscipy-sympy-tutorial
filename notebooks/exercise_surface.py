9748cf6af4e8fa5f78a3f2eb3570530ef35e14d5
Sartaj Singh <singhsartaj94@gmail.com>
2017-08-26 03:33:49 +0530

    complete second notebook

---
+++ notebooks/exercise_surface.py
@@ -0,0 +1,2 @@
+expr = 4 * pi * r**2  # EXERCISE: 4 * pi * r**2
+expr.subs({r: r_v})  # EXERCISE: {r: r_v}
---
+++ notebooks/exercise_volume.py
@@ -0,0 +1,2 @@
+r, V = symbols('r, V', positive=True)  # EXERCISE: 'r, V', positive=True
+solveset(V - (S(4) / 3) * pi * r**3, r, domain=S.Reals)  # EXERCISE: V - (S(4) / 3) * pi * r**3, r, domain=S.Reals
